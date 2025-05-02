a4585174640ed35035ac2660abbd26410cde62d9
moshi <sy.41211@gmail.com>
2025-05-02 18:05:59 +0900

    Refactor for v2 update

--- src/aniclustermap/__init__.py
+++ src/aniclustermap/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "1.4.0"
+import aniclustermap.logger as _logger
+import aniclustermap.utils as _utils
+from aniclustermap.main import AniClustermap
+
+__version__ = "2.0.0"
+__all__ = ["AniClustermap"]
+
+_logger.init_null_logger()
+_utils.add_bin_path()
